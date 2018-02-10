7b16a5f43c1fbb8b2062cce65f8c5057f3f75dd9
Simon Knight <simon.knight@launchworks.com>
2018-02-10 15:19:36 +0000

    working simple Cucumber & TestNG test framework

---
+++ src/test/java/stepdefs/stepDefinitions.java
@@ -0,0 +1,26 @@
+package stepdefs;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class stepDefinitions {
+
+
+    @Given("^a start state$")
+    public void a_start_state() throws Throwable {
+
+    }
+
+    @When("^the state is changed$")
+    public void the_state_is_changed() throws Throwable {
+
+    }
+
+    @Then("^I see the state has changed$")
+    public void i_see_the_state_has_changed() throws Throwable {
+
+    }
+
+}
---
+++ src/test/resources/features/simpleTests.feature
@@ -0,0 +1,6 @@
+Feature: simple tests
+
+  Scenario: simple scenario test
+    Given a start state
+    When the state is changed
+    Then I see the state has changed
